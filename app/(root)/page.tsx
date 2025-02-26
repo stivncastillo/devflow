ee99947fed9611a577cbe376105ae9240daf376d
Stiven Castillo <stivencastillo.90@gmail.com>
2025-02-26 12:03:51 -0500

    Add filter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
@@ -6,16 +7,16 @@ import Link from "next/link";
 const questions = [
   {
     _id: "1",
-    title: "How to use Next.js",
+    title: "How to use Next.js and React",
     description: "I want to learn Next.js",
     tags: [
       {
-        _id: "1",
-        name: "Next.js",
-      },
-      {
         _id: "2",
         name: "React",
+      },
+      {
+        _id: "1",
+        name: "Next.js",
       },
     ],
     author: {
@@ -57,11 +58,19 @@ interface SearchParams {
 }
 
 export default async function Home({ searchParams }: SearchParams) {
-  const { query = "" } = await searchParams;
+  const { query = "", filter = "" } = await searchParams;
 
-  const filteredQuestions = questions.filter((question) =>
-    question.title.toLowerCase().includes(query.toLowerCase())
-  );
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query.toLowerCase());
+
+    const matchesFilter = filter
+      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
+      : true;
+
+    return matchesQuery && matchesFilter;
+  });
 
   return (
     <>
@@ -83,7 +92,7 @@ export default async function Home({ searchParams }: SearchParams) {
           otherClasses="flex-1"
         />
       </section>
-      HomeFilter
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
           <h1 key={question._id}>{question.title}</h1>
