d72d928422f3787d1ef2506fe728419c7d0ac002
Stiven Castillo <stivencastillo.90@gmail.com>
2025-04-22 12:21:42 -0500

    Add signng with oauth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import QuestionCard from "@/components/cards/QuestionCard";
+import { auth } from "@/auth";
 
 const questions = [
   {
@@ -52,6 +53,9 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
+  const session = await auth();
+  console.log("session", session);
+
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
---
+++ types/action.d.ts
@@ -0,0 +1,10 @@
+export interface SignInWithOAuthParams {
+  provider: 'google' | 'github';
+  providerAccountId: string;
+  user: {
+    name: string;
+    username: string;
+    email: string;
+    image?: string;
+  };
+}
